4fd91959edd4a210623eab5a3e5f3e68179f35bb
Lai99 <aaaxaaa99@gmail.com>
2016-03-31 03:10:35 +0800

    v0.1

--- test_sample.py
+++ test_sample.py
@@ -1,22 +1,36 @@
-#-------------------------------------------------------------------------------
-# Name:        module1
-# Purpose:
-#
-# Author:      Admin
-#
-# Created:     28/03/2016
-# Copyright:   (c) Admin 2016
-# Licence:     <your licence>
-#-------------------------------------------------------------------------------
 from bs4 import BeautifulSoup
-
-import lxml
 import requests
+import os
+
+
+def download_file():
+    pass
+
 
 def main():
-    url = 'http://google.com'
+    url = 'https://store.line.me/stickershop/product/6153/zh-Hant'
     response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'lxml')
+    soup = BeautifulSoup(response.text, 'html.parser')
+    is_animation = True
+
+    pic_urls = []
+    for tag in soup.find_all("span"):
+        if u'style' not in tag.attrs:
+            continue
+        style = tag['style']
+        l_p = style.rfind('(') + 1
+        r_p = style.rfind(')')
+        sticker = style[l_p:r_p]
+        if is_animation:
+            pic_urls.append(sticker.replace("/stickers", "/animation"))
+    title = soup.title.text.split()[0]
+
+    if not os.path.exists(title):
+        os.mkdir(title)
+
+    download_dir = os.path.join(os.getcwd(), title)
+    for url in pic_urls:
+        download_file(download_dir, url)
 
 if __name__ == '__main__':
     main()
